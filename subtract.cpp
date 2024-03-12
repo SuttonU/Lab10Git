8a5c27c1f499c7209361196fd11a9e9b41d2721d
DavidMathGod <142970924+DavidMathGod@users.noreply.github.com>
2024-03-12 20:51:55 +0000

    Added subtraction

---
+++ subtract.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout << "Please enter two numbers: " << endl;
+    cout << "num1: "; 
+    cin >> num1;
+    cout << "num2: "; 
+    cin >> num2;
+    cout << num1-num2 << endl;
+    return 0;
+}
