1cd8d6509591d917321307f07b45f47812c902d0
Sutton Jones <smjones2@nic.edu>
2024-03-12 13:44:32 -0700

    Created add function in add.cpp

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int num1 = 0, num2 = 0;
+    std::cout<<"num 1: ";
+    std::cin>> num1;
+    std::cout<<"num 2: ";
+    std::cin>> num2;
+    std::cout<<num1<<" + "<< num2 << " = " << num1 + num2 << std::endl; 
+}
